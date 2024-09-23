a7289843835190eb058ad55e4b19a41cb89ef176
charles <ezecharles39@gmail.com>
2024-09-23 03:06:29 +0100

    my-player-app

---
+++ src/components/Player.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+const Player = ({ name, team, nationality, jerseyNumber, age, imageUrl }) => {
+  return (
+    <Card style={{ width: "18rem", marginBottom: "4rem" }}>
+      <Card.Img variant="top" src={imageUrl} alt={name} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>Team: {team}</Card.Text>
+        <Card.Text>Nationality: {nationality}</Card.Text>
+        <Card.Text>Jersey Number: {jerseyNumber}</Card.Text>
+        <Card.Text>Age: {age}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+Player.defaultProps = {
+  imageUrl: "https://via.placeholder.com/150",
+};
+
+export default Player;
---
+++ src/components/PlayersList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Player from "./Player";
+import players from "../players";
+
+const PlayersList = () => {
+  return (
+    <div style={{ display: "inline" }}>
+      {players.map((player) => (
+        <Player
+          key={player.name}
+          name={player.name}
+          team={player.team}
+          nationality={player.nationality}
+          jerseyNumber={player.jerseyNumber}
+          age={player.age}
+          imageUrl={player.imageUrl}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default PlayersList;
